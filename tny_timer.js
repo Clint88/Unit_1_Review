1ddb8ee0600b108a86148814b840c0aa81188acb
Clint <Ccrockett154@west-mec.org>
2020-01-10 14:43:30 -0700

    review done

--- tny_timer.js
+++ tny_timer.js
@@ -9,14 +9,32 @@
    Date:   Jan 9
 
 */
+showClock();
+setInterval("showClock()", 1000);
 
 function showClock(){
-   var thisDay = new Date("May 19, 2018 at 9:31:27");
+   var thisDay = new Date();
+
    var localDate = thisDay.toLocaleDateString();
    var localTime = thisDay.toLocaleTimeString();
-   document.getElementById("currentTime").innerHTML = "<span>" + localDate + "</span>" + "<span>" + localTime + "</span>";
+// this will pull the data of the html into the JS
+   document.getElementById("currentTime").innerHTML = "<span>"+localDate+"</span><span>"+localTime+"</span>";
+   
    nextJuly4(thisDay);
-   var jDate = nextJuly4(thisDay);
+
+   var j4Date = nextJuly4(thisDay);
+
+   j4Date.setHours("21");
+// this will calculate how many days to seconds you have untill the fireworks
+   var daysLeft = (j4Date - thisDay)/(1000*60*60*24);
+   var hrsLeft = (daysLeft - Math.floor(daysLeft))*24;
+   var minsLeft = (hrsLeft - Math.floor(hrsLeft))*60;
+   var secsLeft = (minsLeft - Math.floor(minsLeft))*60;
+// this will say how many days to seconds you have untill the fireworks
+   document.getElementById("dLeft").textContent= Math.floor(daysLeft);
+   document.getElementById("hLeft").textContent= Math.floor(hrsLeft);
+   document.getElementById("mLeft").textContent= Math.floor(minsLeft);
+   document.getElementById("sLeft").textContent= Math.ceil(secsLeft);
 }
 
 
